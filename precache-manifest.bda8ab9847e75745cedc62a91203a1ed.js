75d070a0c217bac4a5ffa93d393c9dbd0a01e59b
Marcel Augsburger <maugsburger@gmail.com>
2020-05-28 00:14:05 -0400

    Updates

---
+++ precache-manifest.bda8ab9847e75745cedc62a91203a1ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bca3b7e69f6a3f842df0e01e482ef79",
+    "url": "/covid_19_chile_projection/index.html"
+  },
+  {
+    "revision": "f404b71d61f5bfc43141",
+    "url": "/covid_19_chile_projection/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f653208a8a38616e9a9f",
+    "url": "/covid_19_chile_projection/static/js/2.43f30292.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/covid_19_chile_projection/static/js/2.43f30292.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f404b71d61f5bfc43141",
+    "url": "/covid_19_chile_projection/static/js/main.fc7cd403.chunk.js"
+  },
+  {
+    "revision": "be61aa368d763a4b5d34",
+    "url": "/covid_19_chile_projection/static/js/runtime-main.f64e335a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid_19_chile_projection/static/media/logo.5d5d9eef.svg"
+  }
+]);
